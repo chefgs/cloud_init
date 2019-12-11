eec06a225b83492635caee8ef3f7949f1b52d5e5
Saravanan <g.gsaravanan@gmail.com>
2019-12-11 09:17:12 +0000

    AWS ec2 kitchen test

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -7,12 +7,16 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
+describe port(80) do
   it { should_not be_listening }
+end
+
+describe file('/etc/mon-agent/') do
+  it { should exist }
 end
